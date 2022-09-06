fd1572747dcddb99324895a5efde6b1418b82ad9
fcarneva <francesco.carnevali@cern.ch>
2022-09-06 16:29:03 +0200

    Updated version of code September 2022

--- crab/macros/writefiles.py
+++ crab/macros/writefiles.py
@@ -19,38 +19,41 @@ else:
     print "You are launching a single sample and not an entire bunch of samples"
     samples.append(dataset)
 
-path = ".."
+path = "."#".."
 
 for sample in samples:
-    if not os.path.exists("./files/"):
-        os.makedirs("./files/")
-    f = open("./files/"+str(sample.label)+".txt", "w")
+    if not os.path.exists("./macros/files/"):
+        os.makedirs("./macros/files/")
+    f = open("./macros/files/"+str(sample.label)+".txt", "w")
     url = os.popen('crab getoutput --xrootd --jobids=1 -d ' + path + '/crab_' + str(sample.label) + '/').readlines()[0]
     print(url)
     s1=url.split(str(os.environ.get('USER')))
-    print(s1[1])
-    s2=s1[1].split('0000')
-
-    path_xrd = 'root://cms-xrd-global.cern.ch//store/user/' + str(os.environ.get('USER')) + s2[0]
-    newurl = 'srm://stormfe1.pi.infn.it/cms/store/user/' + str(os.environ.get('USER')) + s2[0]
+    
+    s2=s1[1].split('/000')
+    
+    path_xrd = 'root://cms-xrd-global.cern.ch//store/user/' + str(os.environ.get('USER')) + s2[0]+"/"
+    newurl = 'srm://stormfe1.pi.infn.it:8444/srm/managerv2?SFN=/cms/store/user/' + str(os.environ.get('USER')) + s2[0]+"/"
     #path_xrd = 'root://cms-xrd-global.cern.ch//store/user/adeiorio/OutDir/JetHT/DataHTB_2016/200511_122826/'
     #newurl = 'srm://stormfe1.pi.infn.it/cms/store/user/adeiorio/OutDir/JetHT/DataHTB_2016/200511_122826/'
 
     print(newurl)
-    
+    cont =True
     i=0
     print('\nChecking files in the folder '+newurl.strip('\n')+'\n')
-    while True:
-        folder = os.popen('eval `scram unsetenv -sh`; gfal-ls '+ newurl.strip('\n')+'000'+str(i)).readlines()
+    while cont:
+        folder = os.popen('eval `scram unsetenv -sh`; gfal-ls '+ newurl.strip('\n')+'000'+str(i)+"/").readlines()
         newpath_xrd = path_xrd.strip('\n')+'000'+str(i)
         if(len(folder)==0):
             print("The folder does not exist: "+ str(folder))
-            break
+            cont=False
+            continue
         print('sottocartella: '+'000'+str(i))
+        print(newpath_xrd)
         for file in range(len(folder)):
             if(folder[file].strip('\n') == 'log'):
                 continue
-            f.write(newpath_xrd+'/'+folder[file]) 
+            f.write(newpath_xrd+'/'+folder[file])
+            
         i+=1
 
     f.close()
--- crab/submit_crab.py
+++ crab/submit_crab.py
@@ -27,7 +27,7 @@ def cfg_writer(sample, isMC, outdir):
     f.write("config.JobType.pluginName = 'Analysis'\n")
     f.write("config.JobType.psetName = 'PSet.py'\n")
     f.write("config.JobType.scriptExe = 'crab_script.sh'\n")
-    f.write("config.JobType.inputFiles = ['crab_script.py','../scripts/haddnano.py', '../scripts/keep_and_drop.txt']\n") #hadd nano will not be needed once nano tools are in cmssw
+    f.write("config.JobType.inputFiles = ['crab_script.py','../scripts/haddnano.py', '../scripts/keep_and_drop.txt','../python/postprocessing/training_MultiClass.py','../python/model/JSON_MultiClass/high_pt_el_merged.json','../python/model/JSON_MultiClass/high_pt_el_resolved.json','../python/model/JSON_MultiClass/high_pt_mu_merged.json','../python/model/JSON_MultiClass/high_pt_mu_resolved.json','../python/model/JSON_MultiClass/low_pt_el_merged.json','../python/model/JSON_MultiClass/low_pt_el_resolved.json','../python/model/JSON_MultiClass/low_pt_mu_merged.json','../python/model/JSON_MultiClass/low_pt_mu_resolved.json']\n") #hadd nano will not be needed once nano tools are in cmssw
     f.write("config.JobType.sendPythonFolder = True\n")
     f.write("config.section_('Data')\n")
     f.write("config.Data.inputDataset = '"+sample.dataset+"'\n")
@@ -36,13 +36,13 @@ def cfg_writer(sample, isMC, outdir):
     f.write("config.Data.inputDBS = 'global'\n")
     if not isMC:
         f.write("config.Data.splitting = 'LumiBased'\n")
-        if sample.year == '2016':
-            f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/ReReco/Final/Cert_271036-284044_13TeV_ReReco_07Aug2017_Collisions16_JSON.txt'\n")
-        elif sample.year == '2017':
-            f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/ReReco/Cert_294927-306462_13TeV_EOY2017ReReco_Collisions17_JSON_v1.txt'\n")
-        elif sample.year == '2018':
-            f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/ReReco/Cert_314472-325175_13TeV_17SeptEarlyReReco2018ABC_PromptEraD_Collisions18_JSON.txt'\n")
-        f.write("config.Data.unitsPerJob = 50\n")
+        if sample.year == 2016:
+            f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Legacy_2016/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt'\n")
+        elif sample.year == 2017:
+            f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions17/13TeV/Legacy_2017/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt'\n")
+        elif sample.year == 2018:
+            f.write("config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt'\n")
+        f.write("config.Data.unitsPerJob = 80\n")
     else:
         f.write("config.Data.splitting = 'FileBased'\n")
         f.write("config.Data.unitsPerJob = 1\n")
@@ -69,7 +69,7 @@ def crab_script_writer(sample, outpath, isMC, modules, presel):
     f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.MCweight_writer import *\n")
     f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.MET_HLT_Filter import *\n")
     f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.HLT_Filter import *\n")
-    f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.preselection import *\n")
+    #f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.preselection import *\n")
     f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.trigger_preselection import *\n")
     f.write("from PhysicsTools.NanoAODTools.postprocessing.modules.common.PrefireCorr import *\n")
     f.write("from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *\n")
@@ -78,21 +78,27 @@ def crab_script_writer(sample, outpath, isMC, modules, presel):
     f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.preselection_Tprime import *\n")
     f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.unpacking_vers2 import *\n")
     f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.GenPart_MomFirstCp import *\n")
+    f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.scoring import *\n")
+    f.write("from PhysicsTools.NanoAODTools.postprocessing.examples.random_cut import *\n")
     #f.write("infile = "+str(sample.files)+"\n")
     #f.write("outpath = '"+ outpath+"'\n")
     #Deafult PostProcessor(outputDir,inputFiles,cut=None,branchsel=None,modules=[],compression='LZMA:9',friend=False,postfix=None, jsonInput=None,noOut=False,justcount=False,provenance=False,haddFileName=None,fwkJobReport=False,histFileName=None,histDirName=None, outputbranchsel=None,maxEntries=None,firstEntry=0, prefetch=False,longTermCache=False)\n")
     if isMC:
         f.write("metCorrector = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", jesUncert='All', redojec=True)\n")
-        f.write("fatJetCorrector = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", jesUncert='All', redojec=True, jetType = 'AK8PFchs')\n")
+        #f.write("fatJetCorrector = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", jesUncert='All', redojec=True, jetType = 'AK8PFchs')\n")
+        f.write("fatJetCorrector = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", jesUncert='All', redojec=True)\n")
         f.write("metCorrector_tot = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", jesUncert='Total', redojec=True)\n")
-        f.write("fatJetCorrector_tot = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", jesUncert='Total', redojec=True, jetType = 'AK8PFchs')\n")
+        #f.write("fatJetCorrector_tot = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", jesUncert='Total', redojec=True, jetType = 'AK8PFchs')\n")
+        f.write("fatJetCorrector_tot = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", jesUncert='Total', redojec=True)\n")
         f.write("p=PostProcessor('.', inputFiles(), '', modules=["+modules+"], provenance=True, fwkJobReport=True, histFileName='hist.root', histDirName='plots', outputbranchsel='keep_and_drop.txt')\n")# haddFileName='"+sample.label+".root'
     else: 
         f.write("metCorrector = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", runPeriod='"+str(sample.runP)+"', jesUncert='All', redojec=True)\n")
-        f.write("fatJetCorrector = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", runPeriod='"+str(sample.runP)+"', jesUncert='All', redojec=True, jetType = 'AK8PFchs')\n")
+        #f.write("fatJetCorrector = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", runPeriod='"+str(sample.runP)+"', jesUncert='All', redojec=True, jetType = 'AK8PFchs')\n")
+        f.write("fatJetCorrector = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", runPeriod='"+str(sample.runP)+"', jesUncert='All', redojec=True)\n")
         f.write("metCorrector_tot = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", runPeriod='"+str(sample.runP)+"', jesUncert='Total', redojec=True)\n")
-        f.write("fatJetCorrector_tot = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", runPeriod='"+str(sample.runP)+"', jesUncert='Total', redojec=True, jetType = 'AK8PFchs')\n")
-        f.write("HLT = HLT_fun('"+str(sample.year)+"', '"+str(sample.runP)+"')\n")
+        #f.write("fatJetCorrector_tot = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", runPeriod='"+str(sample.runP)+"', jesUncert='Total', redojec=True, jetType = 'AK8PFchs')\n")
+        f.write("fatJetCorrector_tot = createJMECorrector(isMC="+str(isMC)+", dataYear="+str(sample.year)+", runPeriod='"+str(sample.runP)+"', jesUncert='Total', redojec=True)\n")
+        #f.write("HLT = HLT_fun('"+str(sample.year)+"', '"+str(sample.runP)+"')\n")
         f.write("p=PostProcessor('.', inputFiles(), '"+presel+"', modules=["+modules+"], provenance=True, fwkJobReport=True, jsonInput=runsAndLumis(), haddFileName='tree_hadd.root', outputbranchsel='keep_and_drop.txt')\n")#
 
     f.write("p.run()\n")
@@ -154,13 +160,19 @@ for sample in samples:
         year = str(sample.year)
         lep_mod = 'lepSF_'+year+'()'
         trg_mod = 'trigSF_'+year+'()'
-        btag_mod = 'btagSF'+year+'()'
+        btag_mod = 'btagSF_'+year+'()'
+        #btag_mod=''
+        print(' no b tag mode')
         met_hlt_mod = 'MET_HLT_Filter_'+year+'()'
         pu_mod = 'puAutoWeight_'+year+'()'
         prefire_mod = 'PrefCorr_'+year+'()'
         if ('Data' in sample.label):
             isMC = False
             presel = "Flag_goodVertices && Flag_globalSuperTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_eeBadScFilter "
+            #presel = "run>=0"
+            #presel = "Flag_goodVertices && Flag_globalSuperTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_eeBadScFilter && Flag_ecalBadCalibFilter"
+            #if sample.year == 2016:
+            # presel = "Flag_goodVertices && Flag_globalSuperTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_eeBadScFilter"
         else:
             isMC = True
             presel = ""
@@ -171,7 +183,13 @@ for sample in samples:
         cfg_writer(sample, isMC, "Tprime")
 
         if isMC:
-            modules = "MCweight_writer(), preselection_Tprime(), GenPart_MomFirstCp(flavour=\"11,12,13,14,15,16,5,6,24,23,25\"), unpacking_MC()"
+            modules = "MCweight_writer(),MET_HLT_Filter("+year+"), preselection_Tprime(), GenPart_MomFirstCp(flavour=\"11,12,13,14,15,16,5,6,24,23,25\"), unpacking_MC(),scoring(),"+pu_mod+", metCorrector(), fatJetCorrector(), metCorrector_tot(), fatJetCorrector_tot()"
+        
+            if year!="2018": 
+                modules = "MCweight_writer(),MET_HLT_Filter("+year+"), preselection_Tprime(), GenPart_MomFirstCp(flavour=\"11,12,13,14,15,16,5,6,24,23,25\"), unpacking_MC(),scoring(),"+pu_mod+","+ prefire_mod+", metCorrector(), fatJetCorrector(), metCorrector_tot(), fatJetCorrector_tot()"
+                
+            #modules = "MCweight_writer(), preselection_Tprime(), GenPart_MomFirstCp(flavour=\"11,12,13,14,15,16,5,6,24,23,25\"), unpacking_MC()"
+            if ("WJets" in sample.label) and ("1200to" in sample.label): modules = "random_cut()," + modules  
             """
             if year != '2018':
                 modules = "MCweight_writer(),  " + met_hlt_mod + ", preselection(), " + lep_mod + ", " + trg_mod + ", " + pu_mod + ", " + btag_mod + ", " + prefire_mod + ", metCorrector(), fatJetCorrector(), metCorrector_tot(), fatJetCorrector_tot()" # Put here all the modules you want to be runned by crab
@@ -180,7 +198,7 @@ for sample in samples:
             """
         else:
             #modules = "HLT(), preselection(), metCorrector(), fatJetCorrector(), metCorrector_tot(), fatJetCorrector_tot()" # Put here all the modules you want to be runned by crab
-            modules = "MCweight_writer(), preselection_Tprime(), GenPart_MomFirstCp(flavour=\"11,12,13,14,15,16,5,6,24,23,25\"), unpacking_Data()"
+            modules = "MET_HLT_Filter("+year+"), preselection_Tprime(),unpacking_Data(),scoring(), metCorrector(), fatJetCorrector(), metCorrector_tot(), fatJetCorrector_tot()"
             
         print "Producing crab script"
         crab_script_writer(sample,'/eos/user/'+str(os.environ.get('USER')[0]) + '/'+str(os.environ.get('USER'))+'/Tprime/nosynch/', isMC, modules, presel)
@@ -197,12 +215,14 @@ for sample in samples:
 
     elif resubmit:
         print "Resubmitting crab jobs..."
-        os.system("crab resubmit -d crab_" + sample.label)
+        os.system("crab resubmit -d crab_" + sample.label+" --maxjobruntime 2750")
 
     elif status:
         print "Checking crab jobs status..."
         os.system("crab status -d crab_" + sample.label)
 
-    elif getout:
+    if getout:
         print "crab getoutput -d crab_" + sample.label + " --xrootd > ./macros/files/" + sample.label + ".txt"
-        os.system("crab getoutput -d crab_" + sample.label + " --xrootd > ./macros/files/" + sample.label + ".txt")
+    
+        os.system("python macros/writefiles.py -d "+sample.label)
+        #os.system("crab getoutput -d crab_" + sample.label + " --xrootd > ./macros/files/" + sample.label + ".txt")
