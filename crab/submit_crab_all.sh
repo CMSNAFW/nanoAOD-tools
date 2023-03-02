2d6452644540fd027b5fc60c060079e390444ec6
fcarneva <francesco.carnevali@cern.ch>
2023-03-02 11:03:23 +0100

    Added 2016 samples

--- crab/submit_crab_all.sh
+++ crab/submit_crab_all.sh
@@ -1,29 +1,43 @@
-#python submit_crab.py -d WJets_2016  -s                                                   
-#python submit_crab.py -d QCD_2016  -s                                                     
-#python submit_crab.py -d TT_Mtt_2016  -s                                                  
-#python submit_crab.py -d Tprime_LH_2016 -s                                                
-#python submit_crab.py -d ST_2016  -s
-#python submit_crab.py -d DataMu_2016  -s
+python submit_crab.py -d WJets_2016preVFP  -s                                                   
+python submit_crab.py -d QCD_2016preVFP  -s                                                     
+python submit_crab.py -d TT_Mtt_2016preVFP  -s                                                  
+python submit_crab.py -d Tprime_LH_2016preVFP -s                                                
+python submit_crab.py -d ST_2016preVFP  -s
+python submit_crab.py -d DataMu_2016preVFP  -s
+python submit_crab.py -d DataEle_2016preVFP  -s
+python submit_crab.py -d DataPh_2016preVFP  -s
+python submit_crab.py -d DataHT_2016preVFP  -s
 
-python submit_crab.py -d WJets_2017  -s                                                   
-python submit_crab.py -d QCD_2017  -s                                                     
-python submit_crab.py -d TT_Mtt_2017  -s                                                  
-python submit_crab.py -d Tprime_LH_2017 -s                                                
-python submit_crab.py -d ST_2017  -s
-python submit_crab.py -d DataMu_2017  -s
-python submit_crab.py -d DataEle_2017  -s
-python submit_crab.py -d DataPh_2017  -s
-python submit_crab.py -d DataHT_2017  -s
+python submit_crab.py -d WJets_2016postVFP  -s
+python submit_crab.py -d QCD_2016postVFP  -s
+python submit_crab.py -d TT_Mtt_2016postVFP  -s
+python submit_crab.py -d Tprime_LH_2016postVFP -s
+python submit_crab.py -d ST_2016postVFP  -s
+python submit_crab.py -d DataMu_2016postVFP  -s
+python submit_crab.py -d DataEle_2016postVFP  -s
+python submit_crab.py -d DataPh_2016postVFP  -s
+python submit_crab.py -d DataHT_2016postVFP  -s
 
 
-python submit_crab.py -d WJets_2018  -s
-python submit_crab.py -d QCD_2018  -s
-python submit_crab.py -d TT_Mtt_2018  -s
+#python submit_crab.py -d WJets_2017  -s                                                   
+#python submit_crab.py -d QCD_2017  -s                                                     
+#python submit_crab.py -d TT_Mtt_2017  -s                                                  
+#python submit_crab.py -d Tprime_LH_2017 -s                                                
+#python submit_crab.py -d ST_2017  -s
+#python submit_crab.py -d DataMu_2017  -s
+#python submit_crab.py -d DataEle_2017  -s
+#python submit_crab.py -d DataPh_2017  -s
+#python submit_crab.py -d DataHT_2017  -s
+
+
+#python submit_crab.py -d WJets_2018  -s
+#python submit_crab.py -d QCD_2018  -s
+#python submit_crab.py -d TT_Mtt_2018  -s
 #python submit_crab.py -d Tprime_LH_2018 -s
-python submit_crab.py -d ST_2018  -s
-python submit_crab.py -d DataMu_2018  -s
-python submit_crab.py -d DataEle_2018  -s
-python submit_crab.py -d DataHT_2018 -s
+#python submit_crab.py -d ST_2018  -s
+#python submit_crab.py -d DataMu_2018  -s
+#python submit_crab.py -d DataEle_2018  -s
+#python submit_crab.py -d DataHT_2018 -s
 
 
 
