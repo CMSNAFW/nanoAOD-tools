c3c282069b4202a88c289bb9c45c999a5f5548f2
fcarneva <francesco.carnevali@cern.ch>
2024-01-03 10:39:02 +0100

    Fixed bug in MET_HLT_filter

--- python/postprocessing/examples/MET_HLT_Filter.py
+++ python/postprocessing/examples/MET_HLT_Filter.py
@@ -11,36 +11,54 @@ class MET_HLT_Filter(Module):
     def endJob(self):
         pass
     def beginJob(self):
-        if self.year == 2017:
-            self.out.branch("HLT_Ele32_WPTight_Gsf","B")
         pass
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        if self.year == 2017:
+            self.out = wrappedOutputTree
+            self.out.branch("HLT_Ele32_WPTight_Gsf","B")
+        else:
+            pass
+
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
         HLT = Object(event, "HLT")
         flag = Object(event, 'Flag')
-        trigobj = Object(event, "TrigObj")
+        trigobj = Collection(event, "TrigObj")
         electrons = Collection(event, "Electron")
         Ele32 = [False]
 
         if(self.year == 2016):
-            good_HLT = HLT.IsoTkMu24 or HLT.TkMu50 or HLT.Mu50 or HLT.IsoMu24 or HLT.Ele27_WPTight_Gsf or HLT.Photon175 or HLT.AK8PFJet360_TrimMass30
+            try:
+                good_HLT = HLT.IsoTkMu24 or HLT.TkMu50 or HLT.Mu50 or HLT.IsoMu24 or HLT.Ele27_WPTight_Gsf or HLT.Photon175 or HLT.AK8PFJet360_TrimMass30
+            except:
+                good_HLT = HLT.IsoTkMu24 or HLT.Mu50 or HLT.IsoMu24 or HLT.Ele27_WPTight_Gsf or HLT.Photon175 or HLT.AK8PFJet360_TrimMass30
             good_MET = flag.goodVertices and flag.globalSuperTightHalo2016Filter and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter and flag.eeBadScFilter and flag.BadPFMuonDzFilter
         elif(self.year == 2017):
-            good_HLT = HLT.Mu50 or HLT.IsoMu27 or HLT.Ele35_WPTight_Gsf or HLT.Photon200 or HLT.AK8PFJet500 or HLT.OldMu100 or HLT.TkMu100
+            try:
+                good_HLT = HLT.Mu50 or HLT.IsoMu27 or HLT.Ele35_WPTight_Gsf or HLT.Photon200 or HLT.AK8PFJet500 or HLT.OldMu100 or HLT.TkMu100
+            except:
+                good_HLT = HLT.Mu50 or HLT.IsoMu27 or HLT.Ele35_WPTight_Gsf or HLT.Photon200 or HLT.AK8PFJet500
             good_MET = flag.goodVertices and flag.globalSuperTightHalo2016Filter and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter and flag.ecalBadCalibFilter and flag.eeBadScFilter and flag.BadPFMuonDzFilter
             if (good_MET) and (not good_HLT) and (HLT.Ele32_WPTight_Gsf_L1DoubleEG):
                 for el in electrons:
                     for obj in trigobj:
                         dR = deltaR(obj.eta,obj.phi,el.deltaEtaSC + el.eta ,el.phi)
-                        if dR < 0.1:
-                            if bin(obj.filterBits)[10]=='1' : 
+                        if (dR < 0.1) and len(bin(obj.filterBits))>12:
+                            if bin(obj.filterBits)[-11]=='1' : 
                                 good_HLT = True
                                 Ele32[0] = True
             Ele32[0] = Ele32[0] or HLT.Ele35_WPTight_Gsf
             self.out.fillBranch("HLT_Ele32_WPTight_Gsf", Ele32[0])
                 
         elif(self.year == 2018):
-            good_HLT = HLT.Mu50 or HLT.IsoMu24 or HLT.OldMu100 or HLT.TkMu100 or HLT.Ele32_WPTight_Gsf or HLT.Photon200 or HLT.AK8PFJet400_TrimMass30 or HLT.AK8PFJet330_TrimMass30_PFAK8BoostedDoubleB_np2 or HLT.AK8PFJet330_TrimMass30_PFAK8BoostedDoubleB_np4 or HLT.AK8PFJet330_TrimMass30_PFAK8BoostedDoubleB_p02
+            try:
+                good_HLT = HLT.Mu50 or HLT.IsoMu24 or HLT.OldMu100 or HLT.TkMu100 or HLT.Ele32_WPTight_Gsf or HLT.Photon200 or HLT.AK8PFJet400_TrimMass30 or HLT.AK8PFJet330_TrimMass30_PFAK8BoostedDoubleB_np2 or HLT.AK8PFJet330_TrimMass30_PFAK8BoostedDoubleB_np4 or HLT.AK8PFJet330_TrimMass30_PFAK8BoostedDoubleB_p02
+            except:
+                good_HLT = HLT.Mu50 or HLT.IsoMu24 or HLT.OldMu100 or HLT.TkMu100 or HLT.Ele32_WPTight_Gsf or HLT.Photon200 or HLT.AK8PFJet400_TrimMass30 or HLT.AK8PFJet330_TrimMass30_PFAK8BoostedDoubleB_p02
+
             good_MET = flag.goodVertices and flag.globalSuperTightHalo2016Filter and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter and flag.ecalBadCalibFilter and flag.eeBadScFilter and flag.BadPFMuonDzFilter
         else:
             print "Please specify the year: possible choices are 2016, 2017 or 2018"
