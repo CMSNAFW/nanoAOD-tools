5399b7757c6ff48658710f000669941c264311b3
acagnotta <antimo.cagnotta@cern.ch>
2024-02-22 19:06:39 +0100

    cmscalculators implementation

--- crab/crab_script_local.py
+++ crab/crab_script_local.py
@@ -1,21 +1,34 @@
 #!/usr/bin/env python3
 import os
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.jme.jetmetHelperRun2 import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.JetVetoMaps_run3 import *
 from PhysicsTools.NanoAODTools.postprocessing.framework.crabhelper import inputFiles,runsAndLumis
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.MCweight_writer import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.MET_Filter import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.preselection import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.GenPart_MomFirstCp import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoprepro_v2 import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoTopcandidate_v2 import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoTopEvaluate_MultiScore import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.SampleIdx import *
-from PhysicsTools.NanoAODTools.postprocessing.modules.common.globalvar import *
-p=PostProcessor('.', ["root://cms-xrd-global.cern.ch//store/mc/RunIISummer20UL18NanoAODv9/WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1_ext1-v2/40000/B39B1386-2DC2-3B46-98BB-D9C4E176E402.root"], 
-                '', modules=[MCweight_writer(), MET_Filter(year = 2018), preselection(), puAutoWeight_2018(), SampleIdx(21007), GenPart_MomFirstCp(flavour='-5,-4,-3,-2,-1,1,2,3,4,5,6,-6,24,-24'),nanoprepro(),nanoTopcand(isMC=1), globalvar(), nanoTopevaluate_MultiScore()], 
-                provenance=True, fwkJobReport=True, haddFileName='tree_hadd.root',histFileName='hist.root', histDirName='plots', 
-                outputbranchsel='../scripts/keep_and_drop.txt', maxEntries = 100)
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.MCweight_writer import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.MET_Filter import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.preselection import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.puWeightProducer import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.GenPart_MomFirstCp import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoprepro_v2 import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoTopcandidate_v2 import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoTopEvaluate_MultiScore_v2 import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.SampleIdx import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.globalvar import *
+# from PhysicsTools.NanoAODTools.postprocessing.modules.common.lumiMask import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.CMSJMECalculatorsFatJet_module import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.CMSJMECalculatorsMET_module import *
+from PhysicsTools.NanoAODTools.postprocessing.modules.jme.CMSJMECalculatorsJet_module import *
+
+
+# metCorrector = createJMECorrector(isMC=False, dataYear="2022", runPeriod='C', jesUncert='All', redojec=True,jetType = 'AK4PFPuppi')
+# fatJetCorrector = createJMECorrector(isMC=False, dataYear="2022", runPeriod='C', jesUncert='All', redojec=True, jetType = 'AK8PFPuppi')
+# metCorrector_tot = createJMECorrector(isMC=False, dataYear="2022", runPeriod='C', jesUncert='Total', redojec=True,jetType = 'AK4PFPuppi')
+# fatJetCorrector_tot = createJMECorrector(isMC=False, dataYear="2022", runPeriod='C', jesUncert='Total', redojec=True, jetType = 'AK8PFPuppi')
+p=PostProcessor('.', ["root://cms-xrd-global.cern.ch//store/mc/Run3Summer22NanoAODv12/QCD-4Jets_HT-1000to1200_TuneCP5_13p6TeV_madgraphMLM-pythia8/NANOAODSIM/JMENano12p5_132X_mcRun3_2022_realistic_v3-v2/60000/b9d48100-f36a-4422-b17c-6605da590baa.root"], 
+                '', modules=[CMSJMECalculatorsFatJet()],
+                # '', modules=[CMSJMECalculatorsJet()],
+                # '', modules=[CMSJMECalculatorsMET()],
+                provenance=True, fwkJobReport=True, haddFileName='tree_hadd.root' , histFileName='hist.root', histDirName='plots',
+                outputbranchsel='../scripts/keep_and_drop.txt', maxEntries=10)
 p.run()
 print('DONE')
---
+++ python/postprocessing/modules/jme/CMSJMECalculatorsFatJet_module.py
@@ -0,0 +1,69 @@
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+from CMSJMECalculators import loadJMESystematicsCalculators
+from CMSJMECalculators.utils import (
+    toRVecFloat,
+    getJetMETArgs,
+    getFatJetArgs,
+    configureCalc,
+    configureFatJetCalc,
+)
+from CMSJMECalculators import config as calcConfig
+from CMSJMECalculators.jetdatabasecache import JetDatabaseCache
+
+
+
+class CMSJMECalculatorsFatJet(Module):
+    def __init__(self):
+        self.jecDBCache = JetDatabaseCache("JECDatabase", repository="cms-jet/JECDatabase", cachedir="./", mayWrite=True)
+        self.jrDBCache = JetDatabaseCache("JRDatabase", repository="cms-jet/JRDatabase", cachedir="./", mayWrite=True)
+        pass
+
+    def beginJob(self):
+        pass
+
+    def endJob(self):
+        pass
+
+    def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        # self.out = wrappedOutputTree
+        # self.out.branch("EventMass", "F")
+        pass
+
+    def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+        pass
+
+    def analyze(self, event):
+        """process event, return True (go to next module) or False (fail, go to next event)"""
+        
+        config = calcConfig.FatJetVariations()
+        configureCalc(config, jetType="AK8PFPuppi", jerTag="Summer22EEPrompt22_JRV1_MC", splitJER=False,
+                jecTag="Summer22EE_22Sep2023_V2_MC", levels=["L1FastJet", "L2Relative", "L3Absolute"],
+                uncSources=["Total"],
+                jecDBCache=self.jecDBCache, jrDBCache=self.jrDBCache)        
+        configureFatJetCalc(config, isMC=True, year="2022", doSmearing=True, isTau21DDT=False,
+                    jecDBCache=self.jecDBCache, jrDBCache=self.jrDBCache)
+        loadJMESystematicsCalculators()
+        jer = config.create()
+
+        res = jer.produce(*getFatJetArgs(event._tree, isMC=True, addHEM2018Issue=False))
+        print(jer.available())
+        for i in range(jer.available().size()):
+            print(res.pt(i))
+        
+        # res = jer.produce( toRVecFloat(event.Jet_pt),
+        # toRVecFloat(event.Jet_eta),
+        # toRVecFloat(event.Jet_phi),
+        # toRVecFloat(event.Jet_mass),
+        # toRVecFloat(event.Jet_rawFactor),
+        # toRVecFloat(event.Jet_area))
+
+        return True
+
+
+# DATA ["L1FastJet", "L2Relative", "L3Absolute", "L2L3Residual"], UncSources = "Total"
+
+# Summer22EEPrompt22_JRV1_MC, Summer22EE_22Sep2023_V2_MC
+# JR_Winter22Run3_V1_MC, Summer22_22Sep2023_V2_MC
