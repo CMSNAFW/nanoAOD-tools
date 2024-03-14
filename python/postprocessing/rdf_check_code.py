2239eb9e6b9919caa9fb124f8abb20db5fb467df
acagnotta <antimo.cagnotta@cern.ch>
2024-03-14 16:46:38 +0100

    updating folder

--- python/postprocessing/rdf_check_code.py
+++ python/postprocessing/rdf_check_code.py
@@ -4,6 +4,14 @@ from samples.samples import *
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Event, Collection,Object
 from PhysicsTools.NanoAODTools.postprocessing.framework.treeReaderArrayTools import *
 from datetime import datetime
+import optparse
+import math
+import sys
+
+usage = 'python rdf_check_code.py -d dataset'
+parser = optparse.OptionParser(usage)
+parser.add_option('-d', '--dat', dest='dat', type=str, default = '', help='Please enter a dataset name')
+(opt, args) = parser.parse_args()
 
 def HEMveto(jets, electrons):
     hemvetoetaup = -3.05
@@ -50,27 +58,55 @@ def atleast1Ak8good(fatjets):
            b = True
     return b
 
+if not(opt.dat in sample_dict.keys()):
+    print( sample_dict.keys())
+dataset = sample_dict[opt.dat]
 
-file = "root://stormgf2.pi.infn.it//store/user/acagnott/DM_Run3_v0/MET/DataHTC_2018/230921_090909/0000/tree_hadd_118.root"
-# inputFile = ROOT.TFile.Open(file)
-chain = ROOT.TChain("Events")
-chain.Add(file)
+if hasattr(dataset, 'components'): # How to check whether this exists or not
+    print("please enter a single subsample")
+    sys.exit()
+else:
+    print("You are launching a single sample and not an entire bunch of samples")
+    s = dataset
+
+chain = ROOT.TChain('Events')
+ 
+folder_files = "../../crab/macros/files/"
+infile_string = open(folder_files+s.label+".txt")
+file_list = infile_string.readlines()
+
+print("Adding %s to the chain" %(len(file_list)))
+for infile in file_list: 
+    chain.Add(infile)
 
 tree = InputTree(chain)
 nevents = tree.GetEntries()
-sample = sample_dict['DataHTC_2018']
+print("Number of events in chain " + str(chain.GetEntries()))
+print("Number of events in tree from chain " + str((chain.GetTree()).GetEntries()))
 
-passHLTMET = 0
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#+++++++++++++++++++++++++++ Defining histos ++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+hist_workflow = ROOT.TH1F("workflow", "workflow", 10, -0.5, 10)
+hist_workflow.GetXaxis().SetBinLabel(1, "All events")
+hist_workflow.GetXaxis().SetBinLabel(2, "HEM veto")
+hist_workflow.GetXaxis().SetBinLabel(3, "MET Filt")
+hist_workflow.GetXaxis().SetBinLabel(4, "HLT")
+hist_workflow.GetXaxis().SetBinLabel(5, "AH")
+hist_workflow.GetXaxis().SetBinLabel(6, "SL")
+
+hist_met = ROOT.TH1F("MET_pt", "MET p_{T}", 8, 0, 800)
+hist_deltaphi = ROOT.TH1F("MinDelta_phi", "min #Delta #phi", 18, 0, math.pi)
+
+passMETfilt = 0
+passHLT = 0
 passHEMveto = 0
-passLepVeto = 0
-passAK4 = 0
-passAK8 = 0
-passMET = 0
 
-if 'Data' in sample.label: isMC = False
+if 'Data' in s.label: isMC = False
 else: isMC = True
 
-print("sample: ", sample.label, file)
+print("sample: ", s.label)
 print("isMC: ", isMC)
 print("starting events loop : ", datetime.now().strftime("%H:%M:%S"))
 
@@ -81,52 +117,54 @@ for i in range(nevents):
     jets = Collection(event, "Jet")
     fatjets = Collection(event, "FatJet")
     met = Object(event, "MET")
+    mindeltaphi = Object(event, "MinDelta")
     electron = Collection(event, "Electron")
     muon = Collection(event, "Muon")
     HLT = Object(event, "HLT")
     flag = Object(event, "Flag")
-    #run = Object(event, "run")
     nelectron = len(electron)
     nmuon = len(muon)
 
+    hist_workflow.AddBinContent(1)
+
     passesMETHEMVeto = HEMveto(jets, electron)
-    if(sample.year == 2018 and not passesMETHEMVeto):
+    if(s.year == 2018 and not passesMETHEMVeto):
         if(not isMC and chain.run > 319077.):
             continue
-        elif(isMC):
-            w_nominal_nominal[0] *= 0.354
+        # elif(isMC):
+        #     w_nominal_nominal[0] *= 0.354
+    hist_workflow.AddBinContent(2)
     passHEMveto +=1
 
-    good_HLT = HLT.PFMET120_PFMHT120_IDTight or HLT.PFMETNoMu120_PFMHTNoMu120_IDTight
     good_MET = flag.goodVertices and flag.globalSuperTightHalo2016Filter and flag.HBHENoiseFilter and flag.HBHENoiseIsoFilter and flag.EcalDeadCellTriggerPrimitiveFilter and flag.BadPFMuonFilter and flag.ecalBadCalibFilter and flag.eeBadScFilter #and flag.BadPFMuonDzFilter
+    if not (good_MET):
+        continue
+    hist_workflow.AddBinContent(3)
+    passMETfilt += 1
+
+    good_HLT = HLT.PFMET120_PFMHT120_IDTight or HLT.PFMETNoMu120_PFMHTNoMu120_IDTight
     if not (good_HLT and good_MET):
         continue
-    passHLTMET += 1
+    hist_workflow.AddBinContent(4)
+    passHLT += 1
 
-    if not leptonveto(electron, muon):
-        continue
-    passLepVeto += 1
+    hist_met.Fill(met.pt)
+    hist_deltaphi.Fill(mindeltaphi.phi)
 
-    if not atleast1Ak4good(jets):
-        continue
-    passAK4 += 1
-
-    if not atleast1Ak8good(fatjets):
-        continue
-    passAK8 += 1
-
-    if not met.pt>200:
-        continue
-    passMET += 1
+    
+    
 
 print("Ending events loop : ", datetime.now().strftime("%H:%M:%S"))
-print("HLT and MET Efficiency: ", passHLTMET/nevents)
-print("HEM Veto Efficiency: ", passHEMveto/passHLTMET)
-print("Lepton Veto Efficiency: ", passLepVeto/passHEMveto)
-print("AK4 Efficiency: ", passAK4/passLepVeto)
-print("AK8 Efficiency: ", passAK8/passAK4)
-print("MET Efficiency: ", passMET/passAK8)
-print("Total Efficiency: ", passMET/nevents)
+print("HEM Veto Efficiency: ", passHEMveto/nevents)
+print("MET filt Efficiency: ", passMETfilt/passHEMveto)
+print("HLT Efficiency: ", passHLT/passMETfilt)
+print("Total Efficiency: ", passHLT/nevents)
 
 print("\n Total Events (start): ", nevents)
-print("Total Events Select", passMET)
+print("Total Events Select", passHLT)
+
+outfile = ROOT.TFile("/eos/home-a/acagnott/DarkMatter/nosynch/pycodecheck/"+s.label+".root", "recreate")
+hist_workflow.Write()
+hist_met.Write()
+hist_deltaphi.Write()
+outfile.Close()
