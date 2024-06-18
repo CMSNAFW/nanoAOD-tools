b3d25d4d5fb2288770e03c20dda6d70d4f2467e5
acagnotta <antimo.cagnotta@cern.ch>
2024-06-18 17:19:53 +0200

    crab venv setting

--- crab/crab_cfg.py
+++ crab/crab_cfg.py
@@ -2,23 +2,23 @@ from WMCore.Configuration import Configuration
 
 config = Configuration()
 config.section_('General')
-config.General.requestName = 'DataMETD_2018'
+config.General.requestName = 'TprimeToTZ_700_2022'
 config.General.transferLogs=True
 config.section_('JobType')
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'PSet.py'
 config.JobType.maxJobRuntimeMin = 2700
 config.JobType.scriptExe = 'crab_script.sh'
-config.JobType.inputFiles = ['crab_script.py','../scripts/haddnano.py', '../scripts/keep_and_drop.txt']
+config.JobType.inputFiles = ['crab_script.py','../scripts/haddnano.py', '../scripts/keep_and_drop.txt', './Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt', './Cert_Collisions2022_355100_362760_Golden.json', './Cert_Collisions2023_366442_370790_Golden.json', '../../../install_cmssw.sh']
+config.JobType.sendVenvFolder = True
 config.section_('Data')
-config.Data.inputDataset = '/MET/Run2018D-UL2018_MiniAODv2_NanoAODv9_GT36-v1/NANOAOD'
+config.Data.inputDataset = '/TprimeBtoTZ_M-700_LH_TuneCP5_13p6TeV_madgraph-pythia8/Run3Summer22NanoAODv12-130X_mcRun3_2022_realistic_v5-v3/NANOAODSIM'
 config.Data.allowNonValidInputDataset = True
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.lumiMask = ''
 config.Data.unitsPerJob = 1
 config.Data.outLFNDirBase = '/store/user/%s/%s' % ('acagnott', 'DM_Run3_v0')
 config.Data.publication = False
-config.Data.outputDatasetTag = 'DataMETD_2018'
+config.Data.outputDatasetTag = 'TprimeToTZ_700_2022'
 config.section_('Site')
 config.Site.storageSite = 'T2_IT_Pisa'
--- crab/crab_script.sh
+++ crab/crab_script.sh
@@ -15,29 +15,18 @@ echo $PWD
 rm -rf $CMSSW_BASE/lib/
 rm -rf $CMSSW_BASE/src/
 rm -rf $CMSSW_BASE/external/
-ls $CMSSW_BASE/install/
 rm -rf $CMSSW_BASE/install/
-rm -rf $CMSSW_BASE/module/
-rm -rf $CMSSW_BASE/python/
+mv install $CMSSW_BASE/install
 mv external $CMSSW_BASE/external
 mv lib $CMSSW_BASE/lib
 mv src $CMSSW_BASE/src
-mv install $CMSSW_BASE/install
 mv module $CMSSW_BASE/module
 mv python $CMSSW_BASE/python
-mv install_cmssw.sh $CMSSW_BASE/src/
-cd $CMSSW_BASE/src
-rm -r CMSJMECalculators
-git clone https://gitlab.cern.ch/cms-analysis/general/CMSJMECalculators.git
-cd CMSJMECalculators/
-git fetch "https://:@gitlab.cern.ch:8443/aguzel/CMSJMECalculators.git" 'CMSJMECalculators-correctionlib'
-git checkout -b 'CMSJMECalculators-CMSJMECalculators-correctionlib' FETCH_HEAD
-cd ..
-source $CMSSW_BASE/src/install_cmssw.sh
 cmsenv
-cd /srv
+scram-venv
+cmsenv
 echo Found Proxy in: $X509_USER_PROXY
 which python3
-python3 crab_script.py $1
+python3 -c 'from CMSJMECalculators import loadJMESystematicsCalculators;print('ok')'
 hadd tree_hadd.root tree.root hist.root
 fi
