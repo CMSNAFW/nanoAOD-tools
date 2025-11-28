af841bada90842343186cfbf81efb5fcc712c1f9
Francesca Scrivano <fscrivan@lxplus983.cern.ch>
2025-11-28 10:57:28 +0100

    postprocessor modified for the 2024 samples

--- python/postprocessing/run_postprocessor.py
+++ python/postprocessing/run_postprocessor.py
@@ -1,3 +1,4 @@
+"""
 #!/usr/bin/env python3
 import os, sys
 import ROOT
@@ -19,5 +20,24 @@ if 'QCD' in fnames[0]:
     p=PostProcessor(".",fnames,modules=[MCweight_writer(), preselection(), nanoTopcand(), nanoTopevaluate()],  provenance=True, histFileName="histOut"+label+".root", histDirName="plots", maxEntries=10)
 else:
     p=PostProcessor(".",fnames,modules=[MCweight_writer(), preselection(), GenPart_MomFirstCp(flavour="-5,-4,-3,-2,-1,1,2,3,4,5,6,-6,24,-24"),nanoprepro(),nanoTopcand(), nanoTopevaluate()], provenance=True, histFileName="histOut"+label+".root", histDirName="plots", maxEntries=10)
+p.run()
+"""
+#new modules and postprocessor for 2024 samples
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module 
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object 
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoTopcandidate_v2 import nanoTopcand 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoprepro_v2 import nanoprepro 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.GenPart_MomFirstCp import GenPart_MomFirstCp 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.nanoTopEvaluate_MultiScore_v2 import nanoTopevaluate_MultiScore from PhysicsTools.NanoAODTools.postprocessing.modules.common.collectionMerger import collectionMerger 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.MCweight_writer import MCweight_writer 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.Selection import * 
+from PhysicsTools.NanoAODTools.postprocessing.modules.common.preselection import *
+
+#Postprocessor for the Data with option isMC = 0
+#p = PostProcessor("", files, cut="",   modules=[Selection(), preselection(),  nanoTopcand(isMC=0),nanoTopevaluate_MultiScore(year = 2022, isMC=0)], noOut=False, haddFileName="", histFileName="", histDirName="./")
+#p.run()
+#Postprocessor for the MonteCarlo samples
+p = PostProcessor("", files, cut="", modules=[Selection(), preselection(),  GenPart_MomFirstCp(), nanoprepro(),nanoTopcand(),nanoTopevaluate_MultiScore(year = 2022) ], noOut=False, haddFileName="", histFileName="", histDirName="./")
 p.run()
 
